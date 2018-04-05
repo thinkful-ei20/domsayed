d41cb40ba77d0c49bcf49b98c2fe5fb08a8743ce
Dominick McKoy <dominick.mckoy@gmail.com>
2018-04-05 15:26:25 -0400

    fixed linter issues

--- shoppingCartApp/index.js
+++ shoppingCartApp/index.js
@@ -17,11 +17,11 @@ $(function() {
         </button>
       </div>
     </li>`
-    )
+    );
   });
   $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
     $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-  })
+  });
   $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
     $(this).closest('li').remove();
   });
