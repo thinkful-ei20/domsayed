6a2a36a23991b929130592228dccf6f6c4f0cdf1
Sayed Khan <Arsalon47@gmail.com>
2018-04-05 15:09:03 -0400

    Shopping cart app done

---
+++ shoppingCartApp/index.js
@@ -0,0 +1,29 @@
+'use strict';
+$(function() {
+  console.log('jquery test passed');
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    const listItem = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val('');
+
+    $('.shopping-list').append(
+      `<li>
+      <span class="shopping-item">${listItem}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`
+    )
+  });
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  })
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  });
+});
